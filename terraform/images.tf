b0e4535971b3e1b67a3ed3546b9a6db2b00682fc
shamsmosowi <shams.mosowi@gmail.com>
2021-11-28 23:14:19 +0700

    update image

--- terraform/images.tf
+++ terraform/images.tf
@@ -1,4 +1,4 @@
 locals {
  rowy_run_image = "gcr.io/rowy-run/rowy-run@sha256:213208ae404e08d5a1c01147d2555b467cce85a7ccc2fa635447f54bf3ccfc7d"
- rowy_hooks_image = "gcr.io/rowy-run/rowy-hooks@sha256:c52dbc5d7e215c3b155fae738c21629761358a0724ce0077611e0b1e96822"
+ rowy_hooks_image = "gcr.io/rowy-run/rowy-hooks@sha256:c52dbc5d7e215c3b155fae738c21629761358a0724ce0077611e0b1e968222ac"
 }
