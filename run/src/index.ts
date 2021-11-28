8e592e43b1c151115714303da6da358ac46d59bd
shamsmosowi <shams.mosowi@gmail.com>
2021-11-28 16:21:28 +0700

    redirect

--- run/src/index.ts
+++ run/src/index.ts
@@ -1,30 +1,13 @@
 import express from "express";
 import cors from "cors";
-import { getProjectId } from "./metadataService";
-import { db } from "./firebaseConfig";
+import rowyRedirect from "./rowyRedirect";
 const app = express();
 // json is the default content-type for POST requests
 app.use(express.json());
 
 app.use(cors());
 
-app.get("/", async (req, res) => {
-  const projectId = await getProjectId();
-  try {
-    const settingsDoc = await db.doc(`_rowy_/settings`).get();
-    const rowyRunUrl = settingsDoc.get("rowyRunUrl");
-    const setupCompleted = settingsDoc.get("rowyRunBuildStatus") === "COMPLETE";
-    if (setupCompleted) {
-      res.redirect(`https://${projectId}.rowy.app`);
-    } else {
-      res.redirect(
-        `https://${projectId}.rowy.app/setup?rowyRunUrl=${rowyRunUrl}`
-      );
-    }
-  } catch (error) {
-    res.redirect(`https://${projectId}.rowy.app/setup`);
-  }
-});
+app.get("/", rowyRedirect);
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Rowy-deploy: listening on port ${port}!`);
