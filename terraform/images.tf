cb4f34ff70979b4a3a9bc1aca964d205538a1c79
shamsmosowi <shams.mosowi@gmail.com>
2021-11-28 16:06:56 +0700

    image update

--- terraform/images.tf
+++ terraform/images.tf
@@ -1,4 +1,4 @@
 locals {
- rowy_run_image = "gcr.io/rowy-run/rowy-run@sha256:671de15b1abcc175eb5077196e37617ff64ab510966a71c6838c74f1eaa2d890"
- rowy_hooks_image = "gcr.io/rowy-run/rowy-hooks@sha256:f760fdf2c897eb661dede2ec96bcc069ae4aa5f04c6938ef57238dc802633983"
+ rowy_run_image = "gcr.io/rowy-run/rowy-run@sha256:213208ae404e08d5a1c01147d2555b467cce85a7ccc2fa635447f54bf3ccfc7d"
+ rowy_hooks_image = "gcr.io/rowy-run/rowy-hooks@sha256:c52dbc5d7e215c3b155fae738c21629761358a0724ce0077611e0b1e96822"
 }
