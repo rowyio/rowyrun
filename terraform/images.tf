ac49697882563f0e1b00548ad6a0cf45fa91957c
Shams <shams.mosowi@gmail.com>
2022-02-01 03:21:13 +1100

    Update images.tf

--- terraform/images.tf
+++ terraform/images.tf
@@ -1,4 +1,4 @@
 locals {
- rowy_backend_image = "gcr.io/rowy-run/rowy-backend@sha256:54133172ebfdccbc672f7e608c734b84492ab39bf5474716dca736da61bd3381"
+ rowy_backend_image = "gcr.io/rowy-run/rowy-backend@sha256:84ac0953ac1c00e16b00662e9badcaad39bdc58b0d7b705a0c4c91fb8c874d41"
  rowy_hooks_image = "gcr.io/rowy-run/rowy-hooks@sha256:e551ae29e203e0a3904f981df9cc48ca5a1b31f632fa46769bd29e8c46127101"
 }
